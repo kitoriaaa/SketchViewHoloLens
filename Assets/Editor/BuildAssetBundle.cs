650e09c2ae6665865fea81440d88a8c5a3a942b3
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2020-12-10 18:25:42 +0900

    Add object AsseteBundle Build script

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildAssetBundle
+{
+    [MenuItem("Assets/Build Obj")]
+    static void ObjBuild()
+    {
+        var builds = new List<AssetBundleBuild>();
+
+        var build = new AssetBundleBuild();
+        build.assetBundleName = "object";
+        build.assetNames = new string[1] { "Assets/AssetBundleResources/Object/mesh.obj" };
+
+        builds.Add(build);
+
+        var targetDir = "Assets/AssetBundles";
+        if (!Directory.Exists(targetDir)) Directory.CreateDirectory(targetDir);
+
+
+        BuildPipeline.BuildAssetBundles(targetDir, builds.ToArray(), BuildAssetBundleOptions.None, BuildTarget.WSAPlayer);
+    }
+}
+
