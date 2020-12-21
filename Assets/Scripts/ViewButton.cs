812527aaf3a48332d1e40226da6fb1ca92a4a3f4
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2020-12-21 19:12:04 +0900

    Add object load and show button

---
+++ Assets/Scripts/ViewButton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class ViewButton : MonoBehaviour
+{
+    public IEnumerator View()
+    {
+#if WINDOWS_UWP	
+        string bundlepath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "object");
+#else
+        string bundlepath = Path.Combine(Application.streamingAssetsPath, "object");
+#endif
+        AssetBundleCreateRequest bundleLoadRequest = AssetBundle.LoadFromFileAsync(bundlepath);
+        yield return bundleLoadRequest;
+
+        AssetBundle myLoadedAssetBundle = bundleLoadRequest.assetBundle;
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        AssetBundleRequest assetLoadRequest = myLoadedAssetBundle.LoadAssetAsync<GameObject>("mesh");
+        yield return assetLoadRequest;
+
+        GameObject prefab = assetLoadRequest.asset as GameObject;
+
+        // Add componet test move script
+        prefab.AddComponent<BoxCollider>();
+        prefab.AddComponent<ManipulationHandler>();
+        prefab.AddComponent<BoundsControl>();
+
+        // Setting position
+        Camera cam = Camera.main;
+        prefab.transform.position = cam.WorldToViewportPoint(new Vector3(0f, 0f, 2f));
+
+        // Setting scale
+        prefab.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+    }
+
+    
+    public void OnClick()
+    {
+        StartCoroutine(View());
+    }
+}
+
